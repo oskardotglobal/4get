f786b93315afee6e67ace3711078bf0d53f27d0d
lolcat <will@lolcat.ca>
2023-12-02 19:12:36 -0500

    php piece of shit

--- lib/frontend.php
+++ lib/frontend.php
@@ -1332,7 +1332,7 @@ class frontend{
 			return htmlspecialchars($image);
 		}
 		
-		return "/proxy.php?i=" . urlencode($image) . "&s=" . $format;
+		return "/proxy?i=" . urlencode($image) . "&s=" . $format;
 	}
 	
 	public function htmlnextpage($gets, $npt, $page){
