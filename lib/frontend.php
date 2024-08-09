08ed77f947b2cf1979162228a87ba6a30c583bc7
lolcat <will@lolcat.ca>
2024-08-09 10:35:18 -0400

    fucking .php again

--- lib/frontend.php
+++ lib/frontend.php
@@ -1322,7 +1322,7 @@ class frontend{
 			return htmlspecialchars($image);
 		}
 		
-		return "https://4get.ca/proxy?i=" . urlencode($image) . "&s=" . $format;
+		return "/proxy?i=" . urlencode($image) . "&s=" . $format;
 	}
 	
 	public function htmlnextpage($gets, $npt, $page){
